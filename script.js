b150bb0d26653df3bd5e83589f24b910b5f61ebc
Sasha Poghosyan <sash15xxx@mail.ru>
2022-03-22 22:45:59 +0400

    upload files

---
+++ script.js
@@ -0,0 +1,478 @@
+// function name(){
+//     alert(10)
+// }
+// name()
+
+
+
+// let x= function(y){
+//     alert(y + 5)
+// }
+// x(9);
+
+
+// function x(a , b){
+//     alert(a)
+//     alert(b)
+// }
+// x(5 , 2)
+
+
+// function x(a, b){
+//     return a+b
+// }
+// alert(x(2 , 5))
+
+
+
+// var x = function(){
+//     alert('Artur')
+
+// }
+// x()
+
+
+// var x = (a, b)=>{
+//     alert(a+b)
+// }
+// x(5 , 2)
+
+
+// let x = function (y, z){
+//     return y + z;
+// }
+// let f =x(25, 30);
+// alert(f)
+
+
+
+// let x = function(){
+//     return x % 2===0;
+
+// }
+// alert(x(7))
+
+
+
+// let z = [2,6,4,8,12,-9,0,-60,20,7]; amena mec arjeqn
+//let z = [2,3,5,]; amena mec
+//let z = [4,8,12,-9,0,-60,7];poqrn
+
+
+
+
+
+// let z = [4,8,12,-9,0,-60,7];
+
+// let x =function(){
+//     let v =0;
+//     for(let c =0;c < z.length;c++){
+// if(z[c]%2 === 0){
+//    console.log(v += z[c]);
+// }
+//     }
+// }
+// x()
+
+
+
+// let z = [2,6,4,8,12,-9,0,-60,20,7];
+// let x =function(){
+
+//     for(let i =0;i < z.length;i++){
+// if(z[0] < z[i]){
+//     z[0] = z[i]
+// }
+//     }
+
+//     console.log(z[0]);
+// }  
+// x()
+
+
+
+// let z = [5,6,8,9,7,55,42];
+// let x = function(){
+//     let c = -Infinity;
+//     for(let i = 0; i < z.length;i++){
+//         if(c < z[i]){
+//           c = z[i]
+//         }
+//     }
+//     console.log(c);
+// }
+// x()
+
+
+
+// let z = [5,6,8,9,7,55,42];
+// let x = function(){
+//     let c = Infinity;
+//     for(let i = 0; i < z.length;i++){
+//         if(c > z[i]){
+//           c= z[i]
+//         }
+//     }
+//     console.log(c);
+// }
+// x()
+
+
+
+
+// console.log(Math.ceil(44.1));//kloracnum e depi verev
+// console.log(Math.floor(4.7));//kloracnum e depi nerqev
+// console.log(Math.round(4.4));// kesic bardzr depi verev
+// console.log(Math.random());
+//console.log(Math.round(Math.random()*10))
+//console.log(Math.sqrt(49));// armat a hanum
+// console.log(Math.pow(5 , 3));//astichana barcracnum
+
+
+// var x =+prompt('type your age')
+// x < 18 ? alert('anchapahas') : alert('chapahas')
+
+
+// function x(a){
+//     return a%1 === 1 ? a: a - (a%1) +1
+// }
+// console.log(x(5.2));
+
+
+
+
+// [] mechn 10 hat tiv gtnel ayn tvern voronq 3% mnum e 1 5% 2 mnacord
+//[] gtnet dranc meji kent tveri @ndhanur gumarn
+
+
+
+
+// let arr = [1,6,9,12,17,10,15,14,22,13];
+// function array(){
+//     let z = 0;
+//     let c = 0;
+//     for(let i = 0;i < arr.length;i ++){
+//         if(arr[i]%3 === 1){
+//             z++    
+//         }
+//         else if(arr[i]%5 === 2){
+//             c++
+//         }
+
+//     }
+// console.log(z);
+// console.log(c);
+
+// }
+// array()
+
+
+
+
+
+// let arr = [1,6,9,12,17,10,15,14,22,13];
+// function array(){
+//     let z =0;
+//     for(let i = 0;i < arr.length;i++){
+// if(arr[i]%2 === 1){
+//    z += arr[i]
+
+// }
+//     }
+//    console.log(z);
+// }
+// array()
+
+
+
+//  let arr = [1,6,9,12,17,10,15,14,22,13];
+// function array(){
+//     let result = 0;
+//     for(let i = 0;i < arr.length;i++){
+//         if(arr[i]%2 === 0){
+//      result = result+ arr[i]
+//         }
+//     }
+//     console.log(result);
+// }
+// array()
+
+
+
+// let z = [2,6,4,8,12,-9,0,-60,20,7];
+// let x = function(){
+//     let res = Infinity;
+//     for(let i = 0;i < z.length;i++){
+//         if(res > z[i]){
+//             res=z[i]
+//         }
+//     }
+//     console.log(res);
+// }
+// x()
+
+
+
+// let z = [2,6,4,8,12,-9,0,-60,20,7];
+// let x = function(){
+//     let res = -Infinity;
+//     for(let i = 0;i < z.length;i++){
+//         if(res < z[i]){
+//             res = z[i]
+//         }
+//     }
+//     console.log(res);
+// }
+// x()
+
+
+
+
+// function x(a){
+//     return a%1 === 1 ? a: a - (a%1) +1
+// }
+// console.log(x(5.2));
+
+
+// function x(a){
+//     return a%1 === 1 ?   a: a-(a%1) + 1
+// }
+// console.log(x(8));
+
+
+// let arr = [1, 7, -8, 9, 55, 64, 11, 74, 12, 32, 88, -12, 6, -55, 0];
+// let x = function () {
+//     let res = Infinity;
+//     let res1 = -Infinity;
+//     let z = []
+//     let v = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (res > arr[i]) {
+//             res = arr[i]
+//             z = res
+//         }
+//         else if (res1 < arr[i]) {
+//             res1 = arr[i]
+//             v = res1
+//         }
+
+
+
+//     }
+//     console.log(z, v);
+// }
+// x()
+
+
+
+
+// let x = function (y){
+//     let z =0;
+//     let i = 0;
+//     while(i < y.length){
+//         z= z+y[i];
+//         i++;
+//     }
+//     return z;
+// }
+// alert(x([8,4,6,10]));
+
+
+//inch e popoxakani arjeqn minch nran inch vor arjeq veragreln
+//mat inch operatorner gitenq
+//inchpes karox enq veracel text tvi ev tivn texti
+//inchpes karox enq cikl stexcel mer kodum
+//greq cragir vorn kharcni useric nra tariqn ete tariqn poqr e 12 ic cuyc ta barev text alert ete tariqn mece e kam = 12 ic baic poqr e18 ic cuyc ta inchpes eq alert mnacac bolor depqerum cuyc ta chapahas
+//grel reverse funkcia vorn kvercni zangvac ev veradarcni mi nor [] vori  arjeqnern nuynn en inch  irajin [] inn baic hamakargvac en hakarak orinak reverse([8,2,'barev',4]) petqe veradarcni nor [] nman arjeqov [4,'barev',2,8]
+//greq barev anunov funkcia vor kvercni tveri zangvacn ev kveradaarcni ayd tveric stacac arjeqi ev zangvaci erkarutyan bajanman ardyunqn orinak barev([22,27,12,10,9]) petq e veradarcni 16
+//hashvum e qarakusi armatneri gumarn amboxj tvain zangvaci bolor zuyg tveri hamar
+//kazmel funkcia vorn kunena gorcoxutyun if else i ev kazmel nuyni swich case -ov
+
+
+
+
+
+//1  undefined cons - i depcum voch mi ban
+
+
+//2  + , - , * , / , % 
+
+
+
+
+//3 let x = '100'
+// x = +'100'
+// textn darcnum e tiv
+
+
+
+
+//3 let x = 100;
+//  x = ''+100
+//tivn darcnum e text
+
+
+
+
+
+//4  cikl stexcelu hamar ogtagorcum enq
+
+// for(let i = 0;i < 10;i++){
+// console.log(i);
+// }
+
+
+
+// let i =0;
+// while(i < 10){
+//     i++
+// }
+// console.log(i);
+
+
+
+
+
+//5  greq cragir vorn kharcni useric nra tariqn ete tariqn poqr e 12 ic cuyc ta barev text alert ete tariqn mece e kam = 12 ic baic poqr e18 ic cuyc ta inchpes eq alert mnacac bolor depqerum cuyc ta chapahas
+
+// let x = prompt('Գրեք տարիքը')
+// if(x < 12){
+//     alert('Barev');
+
+// }else if(x >= 12 && x < 18){
+//     alert('inchpes eq');
+// }
+// else{
+//     alert('chapahas')
+// }
+
+
+
+
+
+//6
+
+// let arr = [4,12,'Barev',8]
+// let reverse = function(){
+// let z = []
+// z = arr.reverse()
+
+// console.log(z);
+// }
+
+// reverse()
+
+
+
+
+
+7
+// let arr = [5,6,9,10,15,2,3]
+// let barev = function(){
+//     let z = 0
+//     let x = 0
+//     for(let i = 0;i < arr.length;i++){
+//     arr[i] > z
+//     z += arr[i]
+
+//     }
+//     console.log(z);
+// }
+// barev()
+
+
+
+
+
+// let arr = [5,4,7,8,6];
+//  let z = function(){
+//      let x = 0;
+//      let c = 0;
+//      for(let i =0;i < arr.length;i++){
+//          if(arr[i]%2 === 0){
+
+//             x+=Math.sqrt(arr[i])
+
+//          }
+
+//      }
+//      console.log(x);
+//  }
+// z()
+
+
+
+
+
+
+
+
+
+//9
+
+
+
+
+// if(10 > 5){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+// let x = function(fructs){
+//  switch (fructs){
+// case 1:
+//     alert(10 > 5);
+//     case 2:
+//         alert(10 < 5);
+// default:
+//     alert(andifind)
+//  }
+// }
+// x(1)
+
+
+
+
+//greq barev anunov funkcia vor kvercni tveri zangvacn ev kveradaarcni ayd tveric stacac arjeqi ev zangvaci erkarutyan bajanman ardyunqn orinak barev([22,27,12,10,9]) petq e veradarcni 16
+
+
+
+
+
+
+// let arr = [22, 27, 12, 10, 9];
+// let barev = function () {
+//     let z = 0;
+//     let c = 0;
+//     for (let i = 0; i < arr.length; i++) {
+     
+//        if(arr[i] > z){
+//            c += arr[i]
+
+
+        
+//        }
+
+
+        
+//     }
+//     console.log(c/arr.length);
+// }
+// barev()
+
+
+// var i,j,k,m;
+// for(i=1;i<=5;i++){
+//     document.write(i+' ')
+// m = 4;
+// k = i +m;
+// for(j =1;j<i;j++)
+// {
+//     document.write(k +' ')
+//     m--;
+//     k = k+m;
+// }
+// document.write("<br>")
+// }
